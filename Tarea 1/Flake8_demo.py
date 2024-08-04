3f320fddc724b5de100aee44f298787dbce31a11
Gabriel <gabrielgr01@hotmail.com>
2024-08-03 22:34:12 -0600

    Agregando archivo Flake8_demo.py con tres errores identificables por flake 8

---
+++ Tarea 1/Flake8_demo.py
@@ -0,0 +1,7 @@
+# Código de errores Flake8
+
+b = True
+if (type(b) == bool):  # Primer error: comparar tipos
+    print(1)
+elif (type(b) == int ):  # Segundo error: espacio en blanco
+    print(2)# Tercer error: falta doble espacio en comentario
